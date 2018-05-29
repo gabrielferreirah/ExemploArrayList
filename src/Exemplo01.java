c630f12d47e49d61af9b16a300e258ef2a13cbc3
Gabriel de Oliveira Ferreira <gabrieloferreira@hotmail.com>
2018-05-29 11:23:36 -0300

    CEDUP - 29/05

---
+++ src/Exemplo01.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Gabriel de Oliveira Ferreira
+ */
+public class Exemplo01 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        
+        //adicionar 3 números ao ArrayList
+        numeros.add(10);// adiciona na posição 0
+        numeros.add(20);// adiciona na posição 1
+        numeros.add(30);// adiciona na posição 2
+        
+        System.out.println("Números: ");
+        System.out.println(numeros.get(0));//pega na posição 0
+        System.out.println(numeros.get(1));//pega na posição 1
+        System.out.println(numeros.get(2));//pega na posição 2
+        
+        System.out.println(
+                "Tamanho: " + numeros.size()); // tamanho do ArrayList
+    }
+    
+}
